35c63c8537ee5fb9a89388262c3001865d67024e
Smritee Dahal <smrteedahal31@gmail.com>
2025-02-08 03:16:56 +0200

    commit demo 1

--- Assets/Scripts/Shooting target.cs
+++ Assets/Scripts/Shooting target.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+namespace AG1934
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class NewMonoBehaviourScript : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform[] targetEndPoints;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Adjust the speed for the application.
+        public float speed = 1.0f;
+
+        // The target (cylinder) position.
+        private Transform target;
+
+        private int currentEndPointIndex = 0;
+
+        void Start()
+        {
+            target = targetEndPoints[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Move our position a step closer to the target.
+            var step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+            // Check if the position of the cube and sphere are approximately equal.
+            if (Vector3.Distance(transform.position, target.position) < 0.001f)
+            {
+                // Swap the position of the cylinder.
+                SwapEndPointTarget();
+            }
+
+        }
+
+        void SwapEndPointTarget()
+        {
+            currentEndPointIndex++;
+            currentEndPointIndex %= targetEndPoints.Length;
+            target = targetEndPoints[currentEndPointIndex];
+        }
+
+
+    } 
 }
